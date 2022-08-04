ef017463f887ab554c65dad2f713b2a292512dfb
JYS <dbstjs1452@gmail.com>
2022-08-04 19:43:47 +0900

    JYS 0804 20h

--- pj_django/team2app/urls.py
+++ pj_django/team2app/urls.py
@@ -4,7 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
+    path('search/search_ok/', views.search_ok, name='search_ok'),
     path('review/', views.review, name='review'),
     path('map/', views.map, name='map'),
     path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('healthinfo/', views.healthinfo, name='healthinfo'),
+    path('event/', views.event, name='event'),
+    path('mypage/', views.mypage, name='mypage'),
 ]
--- pj_django/team2app/views.py
+++ pj_django/team2app/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse 
 from django.shortcuts import render
 from django.template import loader
 
@@ -6,7 +7,21 @@ def index(request):
     return render(request,'index.html')
 
 def search(request):
+    input1=request.GET.get("input1")
+    input2=request.GET.get("input2")
+    input3=request.GET.get("input3")
+    print(input1)
+    print(input2)
+    print(input3)
     return render(request,'search.html')
+
+def search_ok(request):
+    # input1=request.POST['input1']
+    # input2=request.POST['input2']
+    # input3=request.POST['input3']
+    name=request.POST['inputname']
+    print(name)
+    return HttpResponseRedirect(reverse('search'))
 
 def review(request):
     return render(request,'review.html')
@@ -17,3 +32,14 @@ def map(request):
 def login(request):
     return render(request,'login.html')
 
+def signup(request):
+    return render(request,'signup.html')
+
+def healthinfo(request):
+    return render(request,'healthinfo.html')
+
+def event(request):
+    return render(request,'event.html')
+
+def mypage(request):
+    return render(request,'mypage.html')
